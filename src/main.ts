da06c96cc1756f06a6675bc3bf0e275ab3faedf7
Neo <neoo29v7@gmail.com>
2026-06-11 11:33:07 +0700

    config bootstrap in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,65 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
+import { parseEnvOrigins } from './utils/parse-env-origins';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
+const getCorsAllowList = () => {
+  return parseEnvOrigins(process.env.CLIENT_URL, process.env.CORS_OTHER_URL);
+};
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  //Cookie parser
+  app.use(cookieParser());
+
+  //CORS
+  const allowList = getCorsAllowList();
+  app.enableCors({
+    origin: (requestOrigin: string, callback) => {
+      if (!requestOrigin) {
+        callback(null, true);
+        return;
+      }
+      if (allowList.includes(requestOrigin)) {
+        callback(null, true);
+        return;
+      }
+
+      //log warning
+      callback(null, false);
+    },
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'Accept',
+      'X-Requested-With',
+    ],
+    credentials: true,
+  });
+
+  //Validation Pipe
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  //API Versioning
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
+  await app.listen(process.env.PORT ?? 8080);
 }
-bootstrap();
+bootstrap().catch((error) => {
+  console.error('Failed to start app: ', error);
+  process.exit(1);
+});
